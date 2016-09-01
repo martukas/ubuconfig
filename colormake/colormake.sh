e8862e92b0b271c35884f87bff49efab5a03ca71
MGS <martukas@gmail.com>
2016-09-02 01:22:35 +0200

    fixed bash script for linux

--- colormake/colormake.sh
+++ colormake/colormake.sh
@@ -4,11 +4,12 @@ if [ "$(uname)" == "Darwin" ]; then
 	PLAYER=afplay       
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
 	PLAYER=play
+	PARAM="-q"
 fi
 
 ruby ~/colormake/colormake.rb $@
 if [ "$?" = 0 ]; then
-	${PLAYER} ~/colormake/cow1.wav
+	${PLAYER} ~/colormake/cow1.wav ${PARAM}
 else
-	${PLAYER} ~/colormake/cat_growl.wav
+	${PLAYER} ~/colormake/cat_growl.wav ${PARAM}
 fi
